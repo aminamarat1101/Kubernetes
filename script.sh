a490b77466c6305410ca58c902be7a5f163d6cd2
aminamarat1101 <aminamaratt@gmail.com>
2025-03-15 05:17:27 +0000

    added

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+
+wget https://get.helm.sh/helm-v3.17.1-linux-amd64.tar.gz
+tar zxf helm-v3.17.1-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin
+
+rm -rf linux-amd64 helm-v3.17.1-linux-amd64.tar.gz
